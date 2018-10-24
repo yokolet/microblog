cb48a20cb88ab8f01859c1f0e2bb2b3ef2aea2d6
YOKO HARADA <yokolet@gmail.com>
2018-10-23 23:53:37 -0400

    fix: fixes wrong database cleaner setup
    This is a small yet important fix to run specs with a correct
    database cleaner setup.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,7 +76,7 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before(:each) do |example|
+  config.around(:each) do |example|
     DatabaseCleaner.cleaning do
       example.run
     end
