8fb5883c9ae8b92439c5e14e1ca57a796e5683a9
YOKO HARADA <yokolet@gmail.com>
2018-10-24 14:58:55 -0400

    feat: adds GET request spec
    The GET request to /api/v1/posts has been added with factory setup.

--- spec/requests/api/v1/api_v1_posts_spec.rb
+++ spec/requests/api/v1/api_v1_posts_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Posts", type: :request do
+  let!(:posts) { create_list(:api_v1_post, 10)}
+
   describe "GET /api_v1_posts" do
-    it "works! (now write some real specs)" do
-      get api_v1_posts_path
+    before { get api_v1_posts_path }
+    it "returns status code 200" do
       expect(response).to have_http_status(200)
+    end
+
+    it "returns posts" do
+      json = JSON.parse(response.body)
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
     end
   end
 end
